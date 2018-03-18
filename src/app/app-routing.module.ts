a2a077b747259698cf34bd10c5a4b93bc92b9f98
Gidon Handler <gideon@ravtech.co.il>
2018-03-19 01:10:34 +0200

    frontend components and  css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ShortenUrlComponent} from "./shorten-url/shorten-url.component";
+import {ReportsComponent} from "./reports/reports.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes:Routes = [
+    {path: '', redirectTo: 'shorten', pathMatch: 'full'},
+    {path: 'shorten', component: ShortenUrlComponent},
+    {path: 'reports', component: ReportsComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
