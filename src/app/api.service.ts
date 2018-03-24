05e2013ee3eef56d3c5d2445c22e3965c29251c9
Gidon <gideon@ravtech.co.il>
2018-03-25 02:10:17 +0300

    reports

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -4,14 +4,19 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ApiService {
 
-  host:string = 'http://localhost/shorten-url-admin';
-  path: string = '/web/url/shorten-url';
+    host:string = 'http://localhost/shorten-url-admin';
+    path:string = '/web/url/shorten-url';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http:HttpClient) {
+    }
 
-  getShortUrl(longUrl: string){
-    longUrl = encodeURIComponent(longUrl);
-   return this.http.get(this.host + this.path + '?long=' + longUrl);
-  }
+    getShortUrl(longUrl:string) {
+        longUrl = encodeURIComponent(longUrl);
+        return this.http.get(this.host + this.path + '?long=' + longUrl);
+    }
+
+    getReports(){
+        return this.http.get(this.host+'/web/urls');
+    }
 
 }
--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../api.service";
 
 @Component({
-  selector: 'app-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+    selector: 'app-reports',
+    templateUrl: './reports.component.html',
+    styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+    reports;
 
-  ngOnInit() {
-  }
+    constructor(public api:ApiService) {
+    }
+
+    ngOnInit() {
+        this.api.getReports().subscribe((res)=> this.reports = res);
+    }
 
 }
